cda1a9e82e13bc3f6bac0cdd07c7ef450523c211
Piton <andre.elias.vieira@gmail.com>
2021-01-10 21:37:28 -0300

    Final touches

--- views/PokeDetails.js
+++ views/PokeDetails.js
@@ -6,7 +6,7 @@ import { Ionicons } from '@expo/vector-icons';
 
 function Thumb (props) {
     return (
-        <Image style={{width: 140, height: 180}} 
+        <Image style={{width: 140, height: 200}} 
         source = {{ 
                 uri: props.url 
             }} />
@@ -40,28 +40,31 @@ export default function PokemonDetails ({navigation :{goBack}, route}) {
         <View style={{ marginTop: 30}}>
             <StatusBar style="auto" />
             {/** Just for the back button */}
-            <TouchableOpacity
-                style={{paddingLeft: 10}}
-                onPress={() => goBack()} >
-                <Ionicons name="md-arrow-back" size={26} color="#000" />
-            </TouchableOpacity>
+            <View style={{ flexDirection:'row',paddingLeft: 10, paddingTop: 30 }}>
+                    <TouchableOpacity
+                        onPress={() => goBack()} >
+                        <Ionicons name="md-arrow-back" size={26} color="#000" />
+                    </TouchableOpacity>
+
+                    <Text style={{ fontSize: 35, color: 'green', textAlign: 'center', fontWeight: "bold", width:300}}>
+                                {
+                                    // HEADER (pokemon's name)
+                                    data.name.toUpperCase()
+                                }
+                    </Text>
+            </View>
             
             <View >
-                <View  style={{ flexDirection: 'column', paddingTop: 50}}>
-                    <Text style={{ fontSize: 35, color: 'green', textAlign: 'center', fontWeight: "bold", paddingBottom: 20}}>
-                        {
-                            // HEADER (pokemon's name)
-                            data.name.toUpperCase()
-                        }
-                    </Text>
+                <View  style={{ flexDirection: 'column', paddingTop: 20}}>
+                    
                     <View style={{ flexDirection: "column", alignItems:"center", alignSelf: 'center'}}>
                         {/** IMAGE (pokemon front image) **/}
                         <Thumb url ={data.sprites.front_default}/>
 
                         {/** TYPEs (all two types) **/}
                         <Text style={{ marginTop: 10,fontSize: 20, color: 'black', textAlign: 'center' }}>Types: {data.types[0].type.name.toUpperCase() + (data.types.length > 1 ? ", " + data.types[1].type.name.toUpperCase() : "")}</Text>
-                        <Text style={{ marginTop: 30,fontSize: 17, color: 'black', textAlign: 'center' }}>List of Attacks: </Text>
-                        <SafeAreaView  style={{ marginTop: 20, flexDirection: 'column', width: 200, height: 250, borderColor:'black', borderWidth:1, borderTopWidth:1,  borderBottomRight:1, borderTopLeft:1}}>
+                        <Text style={{ marginTop: 20,fontSize: 17, color: 'black', textAlign: 'center' }}>List of Attacks: </Text>
+                        <SafeAreaView  style={{ marginTop: 10, flexDirection: 'column', width: 200, height: 250, borderColor:'black', borderWidth:1, borderTopWidth:1,  borderBottomRight:1, borderTopLeft:1}}>
                             {/** ATTACKS LIST (all attacks) **/}
                             <FlatList
                                 scrollEnabled={enableScrollViewScroll}
